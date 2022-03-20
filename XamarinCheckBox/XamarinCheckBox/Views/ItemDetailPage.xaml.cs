f0864ceb9d7de7e99daec1446228132791a71f23
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-03-20 11:48:02 +0000

    Add project files.

---
+++ XamarinCheckBox/XamarinCheckBox/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinCheckBox.ViewModels;
+
+namespace XamarinCheckBox.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
